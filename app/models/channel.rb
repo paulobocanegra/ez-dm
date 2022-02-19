ef5935820e8dd4bae1bc6d61d964b695b6198012
Paulo <paulobocanegra@gmail.com>
2022-02-19 15:02:19 -0800

    Add channels validation & generate invite code

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: channels
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  invite_code :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Channel < ApplicationRecord
+    after_initialize :generate_invite_code
+    validates :name, :invite_code, presence: true
+    validates :keycode, uniqueness: true
+
+    private
+
+    def generate_invite_code
+        self.invite_code ||= SecureRandom::base64(12)
+    end
 end
